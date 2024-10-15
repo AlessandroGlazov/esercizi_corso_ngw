8e3a67070b440498de873ad55abe7be8319c3406
AlessandroGlazov <ale2005well@gmail.com>
2024-10-15 16:29:52 +0200

    lezione 15 10 2024

--- src/app/components/hello-world/hello-world.component.ts
+++ src/app/components/hello-world/hello-world.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hello-world',
   standalone: true,
-  imports: [],
   templateUrl: './hello-world.component.html',
-  styleUrl: './hello-world.component.css'
+  styleUrl: './hello-world.component.css',
+  imports: [CommonModule] 
+
 })
 export class HelloWorldComponent {
 
@@ -14,6 +16,10 @@ export class HelloWorldComponent {
   surname:string = "Glazov";
   age:number=19;
   hobby:string = "Music production, Coding, Singing, Cooking";
+  msgscr: boolean = true
+  isLoading: boolean = false;
+  showText: boolean = false;
+
 
 saluta() {
   console.log("ciao")
@@ -22,4 +28,24 @@ saluta() {
 
 
  }
+
+ ActMsgScr(): void { 
+  this.msgscr = !this.msgscr;
+}
+
+toggleShow(): void {
+  this.isLoading = true;
+  this.showText = false;
+
+  
+  setTimeout(() => {
+    this.isLoading = false;
+    this.showText = true; 
+  }, 3000); 
+}
+
+toggleMsgScr(): void {
+  this.msgscr = !this.msgscr;
+}
+
 }
